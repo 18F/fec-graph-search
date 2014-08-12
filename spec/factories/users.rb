0ecebe2ceab1a851b2bbffd3a2a619831beeb66e
amoose <amos@amosarts.com>
2014-08-12 09:41:48 -0700

    rails_apps_composer: omniauth

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :user do
+    name "MyString"
+    provider "MyString"
+    uid "MyString"
+  end
+end
