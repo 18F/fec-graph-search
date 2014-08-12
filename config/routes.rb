f3dc8cfe8cf2a261ce332042025171566b7eb2bd
amoose <amos@amosarts.com>
2014-08-12 09:42:08 -0700

    rails_apps_composer: add pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'visitors#index'
   get '/auth/:provider/callback' => 'sessions#create'
   get '/signin' => 'sessions#new', :as => :signin
   get '/signout' => 'sessions#destroy', :as => :signout
